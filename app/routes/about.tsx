17afc732d8301841d5461b26042aad8851db2b5c
Samuel Jensen <44519206+nichtsam@users.noreply.github.com>
2023-06-05 22:58:05 +0800

    about

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -12,10 +12,50 @@ export const meta: V2_MetaFunction = () => {
 
 export default function About() {
   return (
-    <div className="flex h-full w-full items-center justify-center">
-      <h1 className="text-3xl font-bold">
-        Hello! here should be some introduction of myself.
-      </h1>
+    <div className="h-full w-full">
+      <section className="container mx-auto p-9">
+        <article className="prose dark:prose-invert lg:prose-xl">
+          <h1>About me</h1>
+          <p>
+            Hello there!
+            <br />
+          </p>
+          <p>
+            My name is Samuel Jensen, one developer with a multicultural
+            background. I was born to German and Polish parents and grew up in
+            Taiwan. This unique blend of cultures has shaped my perspective and
+            instilled a deep appreciation for diversity. Language has always
+            been something special to me. I am most fluent in Chinese, followed
+            by English and German. Recently, I've grown a bit of interest in
+            French. Unfortunately, Polish wasn't part of my upbringing, but I
+            look forward to exploring it one day.
+          </p>
+          <p>
+            I've always been a self-taught developer with a curiosity for
+            technology. I love diving into various aspects of development,
+            whether front-end, back-end, or even with CLIs and tooling. I like
+            trying out different things and playing around with stuff to
+            understand them. The versatility of the developing world continues
+            to amaze and inspire me.
+          </p>
+          <p>
+            My programming journey began with Typescript, which I've been using
+            at work. While it's been a valuable experience, I'd like to step out
+            there and get to know more. Currently, I'm exploring the world of
+            Rust, uncovering its unique capabilities and potential applications.
+            Learning new things and embracing endless possibilities is something
+            I genuinely enjoy.
+          </p>
+          <p>
+            I also go by the name "nichtsam". You can look me up on GitHub. Feel
+            free to explore my repositories and see some weird things I've been
+            working on. Thank you for visiting my corner of the digital world.
+            I'm always excited to collaborate and connect with like-minded
+            individuals, so feel free to reach out and share your thoughts or
+            projects.
+          </p>
+        </article>
+      </section>
     </div>
   );
 }
