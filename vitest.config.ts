ba5dc9fc5555d655fc474d766bbacb21a2128645
Samuel Jensen <44519206+nichtsam@users.noreply.github.com>
2024-02-11 11:31:22 +0100

    tell vitest about paths

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "vitest/config";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  plugins: [tsconfigPaths()],
   test: {
     include: ["app/**/*.test.ts?(x)"],
     coverage: {
