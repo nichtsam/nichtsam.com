dbd4891a460984adc1278507905370d3e80457fe
Samuel Jensen <44519206+nichtsam@users.noreply.github.com>
2024-04-08 16:21:25 +0100

    add remix-flat-routes

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { vitePlugin as remix } from "@remix-run/dev";
+import { flatRoutes } from "remix-flat-routes";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
@@ -7,6 +8,13 @@ export default defineConfig({
     tsconfigPaths(),
     remix({
       serverModuleFormat: "esm",
+
+      ignoredRouteFiles: ["**/*"],
+      routes: async (defineRoutes) => {
+        return flatRoutes("routes", defineRoutes, {
+          ignoredRouteFiles: ["**/*.test.{js,jsx,ts,tsx}"],
+        });
+      },
     }),
   ],
 });
