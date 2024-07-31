aa90b0813435ce1a49185ec7a90d34312433d867
Samuel Jensen <44519206+nichtsam@users.noreply.github.com>
2024-07-31 19:30:35 +0200

    blog back to minimalist style

--- app/routes/_site+/blog.tsx
+++ app/routes/_site+/blog.tsx
@@ -1,9 +1,6 @@
 import { json, type HeadersFunction, type MetaFunction } from "@remix-run/node";
 import { type PostInfo, getPostInfos } from "#app/utils/mdx/blog.server";
-import { getPostThumbnailAlt } from "#app/utils/mdx/blog";
 import { Link, useLoaderData } from "@remix-run/react";
-import { AspectRatio } from "#app/components/ui/aspect-ratio";
-import { CloudinaryImage } from "#app/components/image";
 import { pipeHeaders } from "#app/utils/remix.server";
 import { ServerTiming } from "#app/utils/timings.server";
 
@@ -37,11 +34,11 @@ export const loader = async () => {
 export default function Blog() {
   const data = useLoaderData<typeof loader>();
   return (
-    <div className="container">
-      <ul className="grid gap-12 sm:grid-cols-2 lg:grid-cols-3">
+    <div className="container max-w-prose">
+      <ul className="flex flex-col gap-y-4">
         {data.posts.map((post) => (
           <li key={post.slug}>
-            <PostCard post={post} />
+            <PostItem post={post} />
           </li>
         ))}
       </ul>
@@ -49,45 +46,16 @@ export default function Blog() {
   );
 }
 
-function PostCard({ post }: { post: PostInfo }) {
+function PostItem({ post }: { post: PostInfo }) {
   return (
-    <Link to={post.slug}>
-      <div className="group">
-        <div className="transition-transform duration-500 group-hover:-translate-y-3 group-hover:scale-105">
-          <AspectRatio
-            ratio={16 / 9}
-            className="overflow-hidden rounded-xl bg-gradient-to-br from-muted to-muted-foreground"
-          >
-            <CloudinaryImage
-              id={post.meta.matter.thumbnailId}
-              alt={getPostThumbnailAlt(post.meta.matter)}
-              className="h-full w-full object-cover"
-              sizes={[
-                "(min-width: 1400px) 385px",
-                "(min-width: 1024px) 33vw",
-                "(min-width: 640px) 50vw",
-                "100vw",
-              ]}
-              widths={[320, 640, 960, 1280]}
-            />
-          </AspectRatio>
-        </div>
-
-        <div className="mt-2 transition duration-500 group-hover:-translate-y-2">
-          <div className="mx-0 flex items-center justify-between transition-[margin] duration-500 group-hover:mx-7">
-            <p className="text-sm text-muted-foreground">
-              {post.meta.readingTime.text}
-            </p>
-            <p className="text-sm text-muted-foreground">
-              {post.meta.matter.publishedDate}
-            </p>
-          </div>
-
-          <h3 className="font-bold leading-tight transition-all duration-500 group-hover:ml-7">
-            {post.meta.matter.title}
-          </h3>
-        </div>
-      </div>
-    </Link>
+    <div>
+      <Link
+        to={post.slug}
+        className="inline-block text-xl leading-tight underline-offset-4 hover:underline"
+      >
+        {post.meta.matter.title}
+      </Link>
+      <p className="text-muted-foreground">{post.meta.readingTime.text}</p>
+    </div>
   );
 }
