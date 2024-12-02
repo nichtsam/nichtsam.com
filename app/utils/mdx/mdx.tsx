9911b3329b85976368255891cf4de14ae0033821
Samuel Jensen <44519206+nichtsam@users.noreply.github.com>
2024-12-02 22:26:06 +0800

    useMdxComponent cache

--- app/utils/mdx/mdx.tsx
+++ app/utils/mdx/mdx.tsx
@@ -1,3 +1,4 @@
+import { LRUCache } from 'lru-cache'
 import * as mdxBundler from 'mdx-bundler/client/index.js'
 import { useMemo } from 'react'
 import { BlogImage } from '#app/components/image.tsx'
@@ -17,9 +18,20 @@ function getMdxComponent(code: string) {
 	return MdxComponent
 }
 
+const mdxComponentCache = new LRUCache<
+	string,
+	ReturnType<typeof getMdxComponent>
+>({
+	max: 1000,
+})
+
 export const useMdxComponent = (code: string) => {
 	return useMemo(() => {
+		if (mdxComponentCache.has(code)) {
+			return mdxComponentCache.get(code)!
+		}
 		const component = getMdxComponent(code)
+		mdxComponentCache.set(code, component)
 		return component
 	}, [code])
 }
