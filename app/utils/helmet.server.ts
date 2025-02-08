34525b0f497869a76810081ce9c80b3dd4da6885
Samuel Jensen <44519206+nichtsam@users.noreply.github.com>
2025-02-08 18:10:53 +0100

    helmet v2 but with http-helmet

--- app/utils/helmet.server.ts
+++ app/utils/helmet.server.ts
@@ -1,89 +1,94 @@
+import {
+	type CreateSecureHeaders,
+	createSecureHeaders,
+} from '@mcansh/http-helmet'
 import { env } from './env.server'
 
 const MODE = env.NODE_ENV ?? 'development'
 
-type HelmetHeaders = {
-	'Content-Security-Policy'?: string
-	'Cross-Origin-Opener-Policy'?: string
-	'Cross-Origin-Resource-Policy': string
-	'Origin-Agent-Cluster': string
-	'Referrer-Policy': string
-	'Strict-Transport-Security': string
-	'X-Content-Type-Options': string
-	'X-DNS-Prefetch-Control': string
-	'X-Download-Options'?: string
-	'X-Frame-Options'?: string
-	'X-Permitted-Cross-Domain-Policies': string
-	'X-XSS-Protection': string
-}
-
 export function helmet(
 	type: 'html' | 'nonHtml' | 'nonHtmlWithCors',
 	nonce?: string,
-): HelmetHeaders {
+) {
+	const cspReportOnly = false
+	const cspHeaderName = cspReportOnly
+		? 'Content-Security-Policy-Report-Only'
+		: 'Content-Security-Policy'
+
+	const html = {
+		[cspHeaderName]: {
+			'connect-src': [
+				MODE === 'development' ? 'ws:' : undefined,
+				process.env.SENTRY_DSN ? '*.sentry.io' : undefined,
+				"'self'",
+			],
+			'font-src': ["'self'"],
+			'frame-src': ["'self'"],
+			'img-src': [
+				"'self'",
+				'data:',
+				'avatars.githubusercontent.com',
+				'cdn.discordapp.com',
+				'res.cloudinary.com',
+			],
+			'form-action': ["'self'", 'github.com/login/oauth/authorize'],
+			'script-src': [
+				"'strict-dynamic'",
+				"'self'",
+				`'nonce-${nonce}'`,
+				"'unsafe-inline'", // backward compatibility for 'nonces'
+				'https:', // backward compatibility for 'strict-dynamic'
+				"'unsafe-eval'", // mdx-bundler needs this
+			],
+			'script-src-attr': [`'nonce-${nonce}'`],
+		},
+
+		'Cross-Origin-Opener-Policy': 'same-origin',
+		'Cross-Origin-Resource-Policy': 'same-origin',
+		'Referrer-Policy': 'no-referrer',
+		'Strict-Transport-Security': {
+			maxAge: 15552000,
+			includeSubDomains: true,
+		},
+		'X-Content-Type-Options': 'nosniff',
+		'X-DNS-Prefetch-Control': 'off',
+		'X-Frame-Options': 'SAMEORIGIN',
+		'X-XSS-Protection': '0',
+	} satisfies CreateSecureHeaders
+
+	const nonHtml = {
+		...html,
+	} satisfies CreateSecureHeaders
+
+	const nonHtmlWithCors = {
+		...nonHtml,
+		'Cross-Origin-Resource-Policy': 'cross-origin',
+	} satisfies CreateSecureHeaders
+
 	switch (type) {
-		case 'html':
+		case 'html': {
 			if (!nonce) {
 				throw new Error('nonce missing')
 			}
-			return html(nonce)
-		case 'nonHtml':
-			return nonHtml
-		case 'nonHtmlWithCors':
-			return nonHtmlWithCors
+			const headers = createSecureHeaders(html)
+			headers.set('X-Permitted-Cross-Domain-Policies', 'none')
+			headers.set('Origin-Agent-Cluster', '?1')
+			headers.set('X-Download-Options', 'noopen')
+			return headers
+		}
+		case 'nonHtml': {
+			const headers = createSecureHeaders(nonHtml)
+			headers.delete(cspHeaderName)
+			headers.set('X-Permitted-Cross-Domain-Policies', 'none')
+			headers.set('Origin-Agent-Cluster', '?1')
+			return headers
+		}
+		case 'nonHtmlWithCors': {
+			const headers = createSecureHeaders(nonHtmlWithCors)
+			headers.delete(cspHeaderName)
+			headers.delete('Cross-Origin-Opener-Policy')
+			headers.delete('X-Frame-Options')
+			return headers
+		}
 	}
-}
-
-const html = (nonce: string): HelmetHeaders => ({
-	'Content-Security-Policy': Object.entries({
-		'connect-src': [
-			MODE === 'development' ? 'ws:' : null,
-			process.env.SENTRY_DSN ? '*.sentry.io' : null,
-			"'self'",
-		].filter(Boolean),
-		'font-src': ["'self'"],
-		'frame-src': ["'self'"],
-		'img-src': [
-			"'self'",
-			'data:',
-			'avatars.githubusercontent.com',
-			'cdn.discordapp.com',
-			'res.cloudinary.com',
-		],
-		'form-action': ["'self'", 'github.com/login/oauth/authorize'],
-		'script-src': [
-			"'strict-dynamic'",
-			"'self'",
-			`'nonce-${nonce}'`,
-			"'unsafe-inline'", // backward compatibility for 'nonces'
-			'https:', // backward compatibility for 'strict-dynamic'
-			"'unsafe-eval'", // mdx-bundler needs this
-		],
-		'script-src-attr': [`'nonce-${nonce}'`],
-	})
-		.map(([key, value]) => `${key} ${value.join(' ')};`)
-		.join(''),
-
-	'Cross-Origin-Opener-Policy': 'same-origin',
-	'Cross-Origin-Resource-Policy': 'same-origin',
-	'Origin-Agent-Cluster': '?1',
-	'Referrer-Policy': 'no-referrer',
-	'Strict-Transport-Security': 'max-age=15552000; includeSubDomains',
-	'X-Content-Type-Options': 'nosniff',
-	'X-DNS-Prefetch-Control': 'off',
-	'X-Download-Options': 'noopen',
-	'X-Frame-Options': 'SAMEORIGIN',
-	'X-Permitted-Cross-Domain-Policies': 'none',
-	'X-XSS-Protection': '0',
-})
-
-const nonHtml: HelmetHeaders = html('')
-delete nonHtml['Content-Security-Policy']
-delete nonHtml['Cross-Origin-Opener-Policy']
-delete nonHtml['X-Download-Options']
-delete nonHtml['X-Frame-Options']
-
-const nonHtmlWithCors: HelmetHeaders = {
-	...nonHtml,
-	'Cross-Origin-Resource-Policy': 'cross-origin',
 }
