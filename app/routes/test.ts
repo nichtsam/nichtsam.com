14b28109e8fc4605736c8582f76ec7515f028c64
Samuel Jensen <44519206+nichtsam@users.noreply.github.com>
2025-04-09 04:04:55 +0200

    remove forgotten file

--- app/routes/test.ts
+++
@@ -1,9 +0,0 @@
-import { getSignedUrl } from '#app/utils/storage/presigned.server.ts'
-
-export const loader = () => {
-	return getSignedUrl({
-		method: 'GET',
-		key: 'users/qaadPfiErh_1AYPMuLwtU/user-images/1743982458561-_guLC5MP6VdiPNWdELUv-.png',
-		expires: 30,
-	})
-}
