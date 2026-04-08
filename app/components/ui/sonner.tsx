b3ac363c431394cfb7650ce74b35295c7eb3c8c3
Samuel Jensen <44519206+nichtsam@users.noreply.github.com>
2026-04-08 22:51:05 +0200

    clean up some stuff

--- app/components/ui/sonner.tsx
+++ app/components/ui/sonner.tsx
@@ -1,12 +1,12 @@
-import { useTheme } from 'next-themes'
 import { Toaster as Sonner, type ToasterProps } from 'sonner'
+import { useOptimisticThemeMode } from '#app/utils/theme.tsx'
 
 const Toaster = ({ ...props }: ToasterProps) => {
-	const { theme = 'system' } = useTheme()
+	const theme = useOptimisticThemeMode()
 
 	return (
 		<Sonner
-			theme={theme as ToasterProps['theme']}
+			theme={theme}
 			className="toaster group"
 			style={
 				{
